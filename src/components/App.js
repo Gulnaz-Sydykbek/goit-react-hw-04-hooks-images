7f7c591103e13a2c926e543fd6421de44ea10f44
Gulnaz Sydykbek <75367433+Gulnaz-Sydykbek@users.noreply.github.com>
2021-08-29 17:59:08 +0600

    hw-4

--- src/components/App.js
+++ src/components/App.js
@@ -20,36 +20,30 @@ function App() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    return () => {
-      setPage(1);
-      setImages([]);
-    };
-  }, [imageName]);
-
-  useEffect(() => {
-    const imagesFetchApi = () => {
-      fetchImages(imageName, page)
-        .then(images => {
-          setImages(state => [...state, ...images]);
-          setStatus('resolved');
-        })
-        .catch(error => {
-          setError(error);
-          setStatus('rejected');
-        });
-    };
-
     if (imageName !== '') {
+      setStatus('pending');
+
       imagesFetchApi();
     }
-  }, [imageName, page]);
+  }, [imageName]);
 
-  const loadMorePages = () => {
-    return setPage(state => state + 1);
+  const imagesFetchApi = () => {
+    fetchImages(imageName, page)
+      .then(images => {
+        setImages(state => [...state, ...images]);
+        setPage(state => state + 1);
+        setStatus('resolved');
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      });
   };
 
   const handleFormSubmit = imageName => {
     setImageName(imageName);
+    setImages([]);
+    setPage(1);
   };
 
   const toggleModal = () => {
@@ -80,7 +74,7 @@ function App() {
         <Searchbar onFormSubmit={handleFormSubmit} />
         <ImageGallery images={images} onOpenLargeImg={openLargeImg} />
 
-        {images.length !== 0 && <Button onLoadMoreClick={loadMorePages} />}
+        {images.length > 0 && <Button onLoadMoreClick={imagesFetchApi} />}
 
         {showModal && (
           <Modal onToggleModal={toggleModal} showModal={showModal}>
@@ -93,122 +87,5 @@ function App() {
     );
   }
 }
-
-/*class App extends Component {
-  state = {
-    imageName: '',
-    images: [],
-    status: 'idle',
-    error: null,
-    page: 1,
-    showModal: false,
-    largeImageURL: null,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevState.imageName;
-    const nextName = this.state.imageName;
-
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-
-    if (prevName !== nextName) {
-      this.setState({
-        page: 1,
-        images: [],
-        error: null,
-        status: 'pending',
-      });
-
-      this.imagesFetchApi();
-    }
-
-    if (prevPage !== nextPage) {
-      window.scrollTo({
-        top: document.documentElement.scrollHeight,
-        behavior: 'smooth',
-      });
-    }
-  }
-
-  imagesFetchApi = () => {
-    const { imageName, page } = this.state;
-
-    fetchImages(imageName, page)
-      .then(images => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...images],
-          page: prevState.page + 1,
-          status: 'resolved',
-        }));
-      })
-      .catch(error =>
-        this.setState({
-          error,
-          status: 'rejected',
-        }),
-      );
-  };
-
-  handleFormSubmit = imageName => {
-    this.setState({
-      imageName,
-    });
-  };
-
-  toggleModal = () => {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal,
-    }));
-  };
-
-  openLargeImg = largeImageURL => {
-    this.setState({
-      largeImageURL,
-    });
-
-    this.toggleModal();
-  };
-
-  render() {
-    const { images, error, status, largeImageURL, showModal } = this.state;
-
-    if (status === 'idle') {
-      return <Searchbar onFormSubmit={this.handleFormSubmit} />;
-    }
-
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'rejected') {
-      return <Error message={error.message} />;
-    }
-
-    if (status === 'resolved') {
-      return (
-        <div>
-          <Searchbar onFormSubmit={this.handleFormSubmit} />
-          <ImageGallery images={images} onOpenLargeImg={this.openLargeImg} />
-
-          {images.length !== 0 && (
-            <Button onLoadMoreClick={this.imagesFetchApi} />
-          )}
-
-          {showModal && (
-            <Modal
-              onToggleModal={this.toggleModal}
-              showModal={this.state.showModal}
-            >
-              <img src={largeImageURL} alt="" />
-            </Modal>
-          )}
-
-          <ToastContainer autoClose={3000} />
-        </div>
-      );
-    }
-  }
-}*/
 
 export default App;
